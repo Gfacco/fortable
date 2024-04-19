8a22605dd038dab18df2308ba77468c8a4ca7991
gfacco <gfacco@gmail.com>
2024-04-19 13:32:51 -0700

    feat: add row header functionality

--- src/fortable.f90
+++ src/fortable.f90
@@ -1,29 +1,44 @@
 module fortable
   use iso_varying_string, only: &
-    varying_string, var_str, len, operator(//)
+    varying_string, var_str, len, operator(//), trim
   use strff, only: join, NEWLINE, split_at
   implicit none
   private
 
   public :: tabulate
 contains
-  pure function tabulate(list) result(table)
+  pure function tabulate(list, row_headers) result(table)
     type(varying_string), intent(in) :: list(:,:)
+    logical, optional, intent(in) :: row_headers
     type(varying_string) :: padded_list(size(list, dim=1),size(list, dim=2))
     type(varying_string) :: table
     integer :: max_column_width(size(list, dim=2))
     integer :: lines(size(list, dim=2))
     integer :: i,j
 
-    max_column_width = [(maxval(len(list(:,i))), i=1, size(max_column_width))]
-    lines = [(1, i=1, size(max_column_width))]
-    do i=1, size(list, dim=1)
-      padded_list(i,:) = pad_to(list(i,:), max_column_width, lines)
-    end do
-    table = join([(join(padded_list(i,:)," "), i=1, size(list,dim=1))], NEWLINE)
+    if (present(row_headers)) then
+      if (row_headers) then
+        max_column_width = [(maxval(len(list(:,i))), i=1, size(max_column_width))]
+        lines = [(1, i=1, size(max_column_width))]
+        do i=1, size(list, dim=1)
+          padded_list(i,2:) = pad_to_right(list(i,2:), max_column_width(2:), lines(2:))
+        end do
+        do i=1, size(list, dim=1)
+          padded_list(i,1) = pad_to_left(list(i,1), max_column_width(1), lines(1))
+        end do
+        table = join([(join(padded_list(i,:)," "), i=1, size(list,dim=1))], NEWLINE)
+      end if
+    else
+      max_column_width = [(maxval(len(list(:,i))), i=1, size(max_column_width))]
+      lines = [(1, i=1, size(max_column_width))]
+      do i=1, size(list, dim=1)
+        padded_list(i,:) = pad_to_left(list(i,:), max_column_width, lines)
+      end do
+      table = join([(join(padded_list(i,:)," "), i=1, size(list,dim=1))], NEWLINE)
+    endif
   end function
 
-  elemental function pad_to(string, width, num_lines) result(padded)
+  elemental function pad_to_left(string, width, num_lines) result(padded)
   !taken from https://gitlab.com/everythingfunctional/string_tree -by Brad Richardson
         type(varying_string), intent(in) :: string
         integer, intent(in) :: width, num_lines
@@ -33,7 +48,7 @@ contains
 
         associate(lines => split_at(string, NEWLINE))
             associate(padded_lines => pad_line(lines, width))
-                padded = join([padded_lines, [(var_str(repeat(" ", width)), i = 1, num_lines - size(lines))]], NEWLINE)
+                padded = join([padded_lines, [(var_str(repeat(" ", width)), i = 1, num_lines - size(lines))]], "")
             end associate
         end associate
     contains
@@ -43,6 +58,29 @@ contains
             type(varying_string) :: padded_line
 
             padded_line = line // repeat(" ", width - len(line))
+        end function
+    end function
+
+  elemental function pad_to_right(string, width, num_lines) result(padded)
+  !taken from https://gitlab.com/everythingfunctional/string_tree -by Brad Richardson
+        type(varying_string), intent(in) :: string
+        integer, intent(in) :: width, num_lines
+        type(varying_string) :: padded
+
+        integer :: i
+
+        associate(lines => split_at(string, NEWLINE))
+            associate(padded_lines => pad_line(lines, width))
+                padded = join([padded_lines, [(var_str(repeat(" ", width)), i = 1, num_lines - size(lines))]], "")
+            end associate
+        end associate
+    contains
+        elemental function pad_line(line, width) result(padded_line)
+            type(varying_string), intent(in) :: line
+            integer, intent(in) :: width
+            type(varying_string) :: padded_line
+
+            padded_line = repeat(" ", width - len(line)) // line
         end function
     end function
 
--- test/simple_test.f90
+++ test/simple_test.f90
@@ -14,11 +14,18 @@ module simple_test
 //  "Earth 6371   5973.6    " // NEWLINE &
 //  "Moon  1737   73.5      " // NEWLINE &
 //  "Mars  3390   641.85    " 
+character(len=*), parameter :: SIMPLE_LIST_WITH_ROW_HEADERS = &
+    "Sun   696000 1.9891e+09" // NEWLINE &
+//  "Earth   6371     5973.6" // NEWLINE &
+//  "Moon    1737       73.5" // NEWLINE &
+//  "Mars    3390     641.85" 
 contains
   function test_simple() result(tests)
     type(test_item_t) :: tests
 
-    tests = describe("A simple list", [it("displays properly", check_simple)])
+    tests = describe("A simple list", &
+                        [ it("displays properly", check_simple) &
+                        , it("with row headers displays properly", check_row_headers)])
   end function
 
   function check_simple() result(result_)
@@ -37,6 +44,24 @@ contains
     tabular_string = tabulate(simple_list)
 
 
-    result_ = assert_equals(SIMPLE_LIST_FORMAT, tabular_string, tabular_string)
+    result_ = assert_equals(SIMPLE_LIST_FORMAT, tabular_string)
+  end function
+  function check_row_headers() result(result_)
+    type(result_t) :: result_
+    type(varying_string) :: simple_list(4,3)
+    type(varying_string) :: simple_array(12)
+    type(varying_string) :: tabular_string
+
+
+    simple_array =  [ var_str("Sun"),   var_str("696000"),  var_str("1.9891e+09") &
+                    , var_str("Earth"), var_str("6371"),    var_str("5973.6") &
+                    , var_str("Moon"),  var_str("1737"),    var_str("73.5") &
+                    , var_str("Mars"),  var_str("3390"),    var_str("641.85")]
+    simple_list = reshape(simple_array, shape(simple_list), order=[2,1])                    
+
+    tabular_string = tabulate(simple_list, row_headers=.true.)
+
+
+    result_ = assert_equals(SIMPLE_LIST_WITH_ROW_HEADERS, tabular_string)
   end function
 end module
