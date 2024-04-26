9c931555e12c044b00e385aa65b7ecbcaaa9126a
gfacco <gfacco@gmail.com>
2024-04-26 08:54:11 -0700

    combine padding functions

--- src/fortable.f90
+++ src/fortable.f90
@@ -4,6 +4,9 @@ module fortable
   use strff, only: join, NEWLINE, split_at
   implicit none
   private
+  enum, bind(C)
+      enumerator :: left, right
+  end enum
 
   public :: tabulate
 contains
@@ -21,10 +24,10 @@ contains
         max_column_width = [(maxval(len(list(:,i))), i=1, size(max_column_width))]
         lines = [(1, i=1, size(max_column_width))]
         do i=1, size(list, dim=1)
-          padded_list(i,2:) = pad_to_right(list(i,2:), max_column_width(2:), lines(2:))
+          padded_list(i,2:) = pad_to(list(i,2:), max_column_width(2:), lines(2:), alignment = right)
         end do
         do i=1, size(list, dim=1)
-          padded_list(i,1) = pad_to_left(list(i,1), max_column_width(1), lines(1))
+          padded_list(i,1) = pad_to(list(i,1), max_column_width(1), lines(1), alignment = left)
         end do
         table = join([(join(padded_list(i,:)," "), i=1, size(list,dim=1))], NEWLINE)
       end if
@@ -32,56 +35,51 @@ contains
       max_column_width = [(maxval(len(list(:,i))), i=1, size(max_column_width))]
       lines = [(1, i=1, size(max_column_width))]
       do i=1, size(list, dim=1)
-        padded_list(i,:) = pad_to_left(list(i,:), max_column_width, lines)
+        padded_list(i,:) = pad_to(list(i,:), max_column_width, lines, alignment = left)
       end do
       table = join([(join(padded_list(i,:)," "), i=1, size(list,dim=1))], NEWLINE)
     endif
   end function
 
-  elemental function pad_to_left(string, width, num_lines) result(padded)
+  elemental function pad_to(string, width, num_lines, alignment) result(padded)
   !taken from https://gitlab.com/everythingfunctional/string_tree -by Brad Richardson
         type(varying_string), intent(in) :: string
         integer, intent(in) :: width, num_lines
+        integer, intent(in) :: alignment
         type(varying_string) :: padded
 
         integer :: i
-
-        associate(lines => split_at(string, NEWLINE))
-            associate(padded_lines => pad_line(lines, width))
+        select case(alignment)
+        case(left)
+          associate(lines => split_at(string, NEWLINE))
+            associate(padded_lines => pad_left(lines, width))
                 padded = join([padded_lines, [(var_str(repeat(" ", width)), i = 1, num_lines - size(lines))]], "")
             end associate
-        end associate
+          end associate   
+        case(right)
+          associate(lines => split_at(string, NEWLINE))
+            associate(padded_lines => pad_right(lines, width))
+                padded = join([padded_lines, [(var_str(repeat(" ", width)), i = 1, num_lines - size(lines))]], "")
+            end associate
+          end associate
+        case default   
+        end select
+
     contains
-        elemental function pad_line(line, width) result(padded_line)
+        elemental function pad_left(line, width) result(padded_line)
             type(varying_string), intent(in) :: line
             integer, intent(in) :: width
             type(varying_string) :: padded_line
 
             padded_line = line // repeat(" ", width - len(line))
         end function
-    end function
-
-  elemental function pad_to_right(string, width, num_lines) result(padded)
-  !taken from https://gitlab.com/everythingfunctional/string_tree -by Brad Richardson
-        type(varying_string), intent(in) :: string
-        integer, intent(in) :: width, num_lines
-        type(varying_string) :: padded
-
-        integer :: i
-
-        associate(lines => split_at(string, NEWLINE))
-            associate(padded_lines => pad_line(lines, width))
-                padded = join([padded_lines, [(var_str(repeat(" ", width)), i = 1, num_lines - size(lines))]], "")
-            end associate
-        end associate
-    contains
-        elemental function pad_line(line, width) result(padded_line)
+        elemental function pad_right(line, width) result(padded_line)
             type(varying_string), intent(in) :: line
             integer, intent(in) :: width
             type(varying_string) :: padded_line
 
             padded_line = repeat(" ", width - len(line)) // line
-        end function
+        end function       
     end function
 
 end module fortable
